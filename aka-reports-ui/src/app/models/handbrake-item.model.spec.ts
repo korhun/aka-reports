510d38817ae558b652b33200a278bd9853c0d3be
korhun <korhun@gmail.com>
2022-05-17 19:20:48 +0300

    handbreak-item model added

---
+++ aka-reports-ui/src/app/models/handbrake-item.model.spec.ts
@@ -0,0 +1,7 @@
+import { HandbrakeItem } from './handbrake-item.model';
+
+describe('HandbrakeItem', () => {
+  it('should create an instance', () => {
+    expect(new HandbrakeItem()).toBeTruthy();
+  });
+});
--- aka-reports-ui/src/app/models/handbreak-item.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { HandbreakItem } from './handbreak-item.model';
-
-describe('HandbreakItem', () => {
-  it('should create an instance', () => {
-    expect(new HandbreakItem()).toBeTruthy();
-  });
-});
